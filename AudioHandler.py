25f42da2fdc271cfe22c631e601d9a5af21b9733
andrewbartels1 <46938273+andrewbartels1@users.noreply.github.com>
2019-04-06 10:48:33 -0700

    update

--- AudioHandler.py
+++ AudioHandler.py
@@ -8,36 +8,52 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 import csv
 import os
-import numpy as np
 import glob
-import matplotlib
 import numpy as np
-import matplotlib.pyplot as plt  
+#import matplotlib.pyplot as plt  
+#import tensorflow as tf
+#import keras as ks
 
 
 class AudioHandler():
     
-def __init__(self):
-    mic_text = 'test.txt'
+    def __init__(self, dir1, dir2):
+        self.dir1 = dir1
+        self.dir2 = dir1
+        self.mic_text = 'test.txt'
+        self.Fs          = 48000;                        #  DAQ Sample rate (S/sec)
+        self.NS          = 48000;                        #  Number of samples
+        self.fn          = self.Fs/2;                         #  maximum resolvoble frequency
+        self.NFFT        = 2**12;                        #  4096 point FFT
+        self.NF          = self.NFFT/2;                       #  No. point for powerspecturm
+        self.NR          = 4;                            #  No. of runs
+        self.NP          = 4;                            #  No. of positions
+        self.NM          = 7;                            #  No. of microphones
+        self.Pref        = 20e-6;                        #  Reference pressure
+        self.sensitivity = [50.2, 49.3, 53.5, 52.9, 52.8, 48.7, 47.0] # microphone sensitivity
+
+    # microphone sensitivity
+    mic_sensitivity = np.array([50.2, 49.3, 53.5, 52.9, 52.8, 48.7, 47.0])
+    Pref = 20e-6
     
-# microphone sensitivity
-mic_sensitivity = np.array([50.2, 49.3, 53.5, 52.9, 52.8, 48.7, 47.0])
-Pref = 20e-6
-
-def FileSort(self, dir1, dir2):
-    '''Sorts the files into the different types of tests
-    '''
-    self.TestSectionCalibartion_data = glob.glob(dir1, '?.dat')
-    self.MomentumWakeSheilding_data =  glob.glob(dir2, '?.dat')
-
-def MicCalibartion(self):
-    '''Microphone calibration here
-    '''
+    def FileSort(self):
+        '''Sorts the files into the different types of tests
+        '''
+        self.TestSectionCalibartion_data = glob.glob(self.dir1, '?.dat')
+        self.MomentumWakeSheilding_data =  glob.glob(self.dir2, '?.dat')
+    
+    def MicCalibartion(self):
+        '''Microphone calibration here
+        '''
     
 def main():
-    TestSectionCalibartion_dir = 'C:/Users/andre/Dropbox (CSU Fullerton)/EGME597_AB/RAWDATA/TSC'
-    MomentumWakeSheilding_dir = 'C:/Users/andre/Dropbox (CSU Fullerton)/EGME597_AB/RAWDATA/MWS'
-    
+    path = 'C:\\Users\\andrewbartels1\\Dropbox (CSU Fullerton)\\EGME597_AB'
+    os.chdir(path)
+    TestSectionCalibartion_dir = 'EGME597_AB/RAWDATA/TSC'
+    MomentumWakeSheilding_dir = 'EGME597_AB/RAWDATA/MWS'
+    calibration_dir = 'EGME597_AB/CALIBRATION/sn442'
+    AH = AudioHandler(TestSectionCalibartion_dir, MomentumWakeSheilding_dir)
+    AH.FileSort()
 
 if __name__ == "__main__":
     main()
--- tic_toc.py
+++ tic_toc.py
@@ -4,15 +4,13 @@ Created on Thu Mar 28 22:35:20 2019
 
 @author: andrewbartels1
 """
-
+import time
 def tic():
     #Homemade version of matlab tic and toc functions
-    import time
     global startTime_for_tictoc
     startTime_for_tictoc = time.time()
 
 def toc():
-    import time
     if 'startTime_for_tictoc' in globals():
         print("Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds.")
     else:
