678b91c80f18e8cf76630aada569d689a2eb52b1
Andrew Bartels <andrewbartels1@gmail.com>
2019-04-18 19:03:18 -0700

    working on different plotting

--- preprocessing.py
+++ preprocessing.py
@@ -20,17 +20,34 @@ import h5py
 import glob
 import numpy as np
 import pandas as pd
+import scipy as sp
 import scipy.interpolate as spinterp
+from sklearn.preprocessing import normalize
 from scipy import signal
 from sklearn.decomposition import FastICA, PCA
+import matplotlib.pyplot as plt
 
 # Set home dir
 from os.path import expanduser
 home = expanduser("~")
 
+#   Specify constants
+sampleRate  = 48000                        #  DAQ Sample rate (S/sec)
+NS          = 48000                        #  Number of samples
+fn          = sampleRate/2                 #  maximum resolvoble frequency
+NFFT        = 2**12                        #  4096 point FFT
+NF          = NFFT/2                       #  No. point for powerspecturm
+NR          = 4                            #  No. of runs
+NP          = 4                            #  No. of positions
+NM          = 7                            #  No. of microphones
+Pref        = 20e-6                        #  Reference pressure
+sensitivity = [50.2, 49.3, 53.5, 52.9, 52.8, 48.7, 47.0] # microphone sensitivity
+c = 343
+MicArrayElements = 7
+arraySpacing = 0.00858 # Array spacing (cm)
 
-filename = 'TSC202.hdf5'
-folderPath = 'C:/Users/andrewbartels1/Dropbox (CSU Fullerton)/EGME597_AB/ML_DATA/'
+filename = 'MWS203.hdf5'
+folderPath = 'C:/Users/andre/Dropbox (CSU Fullerton)/EGME597_AB/ML_DATA/'
 fileType = '*.hdf5'
 
 
@@ -51,12 +68,30 @@ field = [i.decode('utf-8') for i in field]
 
 data_dict = {key_list[3]: field,
              key_list[0]: list(CleanedAcousticPressure),
-             key_list[1]: list(RawPressure),
-             key_list[2]: list(SmSpectrum),
              'noiseFlag': np.ones(CleanedAcousticPressure.shape[0])
         }
 
 df = pd.DataFrame(data_dict, columns=key_list)
+
+print(df[key_list[0]].shape)
+
+# Next thing on the list... BSS on each of the different Cleaned pressures and 
+# Labeling all the points. First thing that needs to be done is the 
+
+def normalize_complex_arr(a):
+    a_oo = a - a.real.min() - 1j*a.imag.min() # origin offsetted
+    return a_oo/np.abs(a_oo).max()
+
+def PlotPressure(timeArray, SampleRate):
+    f, t, Sxx = signal.spectrogram(timeArray, SampleRate)
+    plt.pcolormesh(t, f, Sxx)
+    plt.ylabel('Frequency [Hz]')
+    axes = plt.axes()
+    axes.set_ylim([0, 1])
+    plt.xlabel('Time [sec]')
+    plt.show()
+
+PlotPressure(df[key_list[0]][0],SampleRate=2048)    
 
 # =============================================================================
 # list[dicts_of_all_hdf5_files]
